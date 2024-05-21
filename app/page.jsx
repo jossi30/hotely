4a59821297b36debcbf911f8634f061f231f8c04
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-05-21 21:07:17 +0300

    project setup

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Hero from '@/components/Hero';
+
+export default function Home() {
+  return  <main> 
+            <Header />
+            <Hero />
+            <Footer />   
+     </main>
+  
+}
