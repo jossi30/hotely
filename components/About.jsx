005bad6025955861ddb2e4010d39c2d61e0ef0fd
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-06-02 16:56:38 +0300

    completed the features box with content and motion

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Image from "next/image";
-import {motion} from 'framer-motion'
+import {inView, motion} from 'framer-motion'
 import { fadeIn } from "@/variants";
 import { useInView } from "react-intersection-observer";
 import { useMediaQuery } from "react-responsive";
@@ -8,7 +8,16 @@ import { Button } from "./ui/button";
 import Stats from "./Stats";
 
 const About = () => {
-  return ( <section className="py-12">
+    const isMobile = useMediaQuery({
+        query: '(max-width: 768px)',
+    });
+
+    const [ref, inView] = useInView({
+        threshold: !isMobile ? 0.5: null,
+    });
+    
+    
+  return ( <section className="py-12 xl:pt-0 xl:pb-24" ref={ref}>
     <div className="container mx-auto ">
         <div className="flex flex-col xl:flex-row">
             <motion.div 
@@ -16,9 +25,9 @@ const About = () => {
                 initial='hidden'
                 whileInView={'show'}
                 viewport={{ once: false, amount: 0.4}}
-                className=" flex-1 relative"
+                className='flex-1 relative'
                 >
-                <Image src={'/about/img2.png'} width={959} height={1221} alt='' />
+                  <Image src={'/about/img2.png'} width={559} height={721} alt='' />
             </motion.div>
             <motion.div 
                 variants={fadeIn('left',0.5)}
@@ -31,7 +40,7 @@ const About = () => {
                 <p>
                 At j30-Hotely, we redefine the art of hospitality.Our hotels offer a perfect blend of modern elegance and timeless charm. Whether you are here for business, leisure, or a special occasion, we provide an unparalleled experience that will leave you with lasting memories.
                 </p>
-                <div className="my-5 xl:my-10 min-h-[35px]"> <Stats /> </div>
+                <div className="my-5 xl:my-10 min-h-[35px]"> {inView && <Stats />} </div>
                 <p className="mb-10 ">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam numquam dolorum quam impedit obcaecati fugiat quos distinctio! Aspernatur mollitia provident, nesciunt ea consequuntur, ab explicabo laboriosam sequi vero illum harum.
                 </p>
--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,8 +1,29 @@
+'use client'
+import Image from "next/image"
+import { motion } from "framer-motion"
+import { fadeIn } from "@/variants"
+
 const Featured = () => {
   return (
-    <section className="bg-soft_green-secondary xl:h-[240px] max-w-[1305px] ml-auto xl:-top-[120px] relative rounded-tl-[30px] rounded-bl-[30px] px-[80px] py-[60px]">
-      Featured
-    </section>
+    <motion.section
+    variants={fadeIn('right',0.2)}
+    initial='hidden'
+    whileInView={'show'} 
+    viewport={{ once: false, amount: 0.2 }}
+    className="bg-soft_green-secondary xl:h-[240px] max-w-[1305px] ml-auto xl:-top-[120px] relative rounded-tl-[30px] rounded-bl-[30px] px-[80px] py-[60px]">
+      <div className="flex flex-col xl:flex-row items-center h-full gap-x-[30px] text-center xl:text-left">
+        <Image
+        className="xl:mr-[50px]" 
+         src='/featured/icon.svg'
+         width={73} 
+         height={84} 
+         alt='' />
+        <h2 className="h2 flex-1 text-soft_green leading-relaxed">Book an Awesome Room in less than 1 minute.</h2>
+        <p className="flex-1 text-soft_green leading-loose">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat facere sit fuga dolore? Nisi nam id accusantium, consequuntur sequi repellendus, similique earum perferendis ad, enim deserunt facilis tempora tempore illo.
+        </p>
+      </div>
+    </motion.section>
   )
 }
 
