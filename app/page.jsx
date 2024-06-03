5710e46f8d515871666dff62bc682496065267fb
Yosief G. Hailay <yosiefghaylay@gmail.com>
2024-06-03 22:02:11 +0300

    added a section to find all hotels and styled it

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Featured from '@/components/Featured';
 import Recommendation from '@/components/Recommendation';
+import Find from '@/components/Find';
 
 export default function Home() {
   return  <main className='max-w-[1440px] mx-auto bg-white overflow-hidden'> 
@@ -12,6 +13,7 @@ export default function Home() {
             <Featured />
             <About />
             <Recommendation />
+            <Find />
             <Footer /> 
             <div className='h-[4000px]'></div>  
      </main>
